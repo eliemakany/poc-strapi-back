49ff699d48840bee8eb9b4375a7509e2553b497e
Elie MAKANY <elie.makany@adeliom.com>
2021-07-05 15:33:32 +0200

    init

---
+++ app/config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', 'dffdf211eb6e307643634ca9f45b11ea'),
+    },
+  },
+});
---
+++ app/extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || '0e77e13c-a995-4106-a9fa-bcef1f9dc416'
+};
